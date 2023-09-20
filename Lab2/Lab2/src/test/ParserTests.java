bb00e45ee8d98e3aa15cc8dc1eb0890869fe2310
ZLIEW005 <ZLIEW005@e.ntu.edu.sg>
2023-09-20 20:49:28 +0800

    Statements in parser.beaver, tests in ParserTests.java

--- Lab2/Lab2/src/test/ParserTests.java
+++ Lab2/Lab2/src/test/ParserTests.java
@@ -37,4 +37,44 @@ public class ParserTests {
 	public void testEmptyModule() {
 		runtest("module Test { }");
 	}
+	
+	@Test
+	public void testModuleImports() {
+		runtest("module Test {" + "import module1;" + "import module2;" + "}");
+	}
+
+	@Test
+	public void testModuleTypeDecl() {
+		runtest("module Test {" + "public type int = \"INT\";" + "type float = \"FLOAT\";" + "}");
+	}
+
+	@Test
+	public void testModuleFieldDecl() {
+		runtest("module Test {" + "public boolean field1;" + "int[] field2;" + "}");
+	}
+
+	@Test
+	public void testModuleEmptyFuncDecl() {
+		runtest("module Test {" + "public void func() {}" + "}");
+	}
+
+	@Test
+	public void testModuleParamFuncDecl() {
+		runtest("module Test {" + "public void func(boolean param) {}" + "}");
+	}
+
+	@Test
+	public void testModuleParamListFuncDecl() {
+		runtest("module Test {" + "public void func(int param1, boolean param2) {}" + "}");
+	}
+
+	@Test
+	public void testModuleWhileIfFuncDecl() {
+		runtest("module Test {" + "public int func(int param) {" + " while(param < 10)"
+				+ " if(param%2 == 0) {param = param+1;}"
+				+ "	else {param = param*2;}" 
+				+ " return param;"
+				+ "}" + "}");
+	}
+
 }
